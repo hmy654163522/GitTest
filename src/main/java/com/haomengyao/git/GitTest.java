0f9124bd7bc169b9fa7b87a71d0712e9a17f3a35
hmy <654163522@qq.com>
2022-03-25 19:54:27 +0800

    first commit

---
+++ src/main/java/com/haomengyao/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.haomengyao.git;
+
+/**
+ * Author:HaoMengYao
+ * Date:2022/3/2519:49
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
